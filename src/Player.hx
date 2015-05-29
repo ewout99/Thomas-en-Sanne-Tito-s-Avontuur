e037e8fa6d3ed8d94b9fde01df70a523fa240238
TheImperialDonut <kjellneumann@gmail.com>
2015-05-29 11:55:45 +0200

    Random Stuff
    Disregard

--- src/Player.hx
+++ src/Player.hx
@@ -4,12 +4,21 @@ package ;
  * Handles the movement of the character
  * @author Ezzz
  */
+
 class Player
 {
+	var stageY:Int = stage.getheight;
 
 	public function new() 
 	{
+		super();
 		
+		var bitmapData:BitmapData = Assets.getBitmapData ( "assets/tom_tilesheet.png" ) ;
+		var character = new Bitmap ( bitmapData ) ;
+		character.height = ( stageY / 10 ) ;
+		character.width = character. ;
+		
+		addChild (character) ;
 	}
-	
+
 }
